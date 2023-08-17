77e19f9a2b66ec000f6dd67757502578ab6db2d7
149125852 <youssefgamerpro@gmail.com>
2023-08-16 18:54:09 -0700

    a

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,14 +10,14 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+        int i = 0;
 
-	if (array && size && cmp)
-		while (i < size)
-		{
-			if (cmp(array[i]))
-				return (i);
-			i++;
-		}
-	return (-1);
+        if (array && size && cmp)
+                while (i < size)
+                {
+                        if (cmp(array[i]))
+                                return (i);
+                        i++;
+                }
+        return (-1);
 }
--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -10,10 +10,10 @@
  * @op: the operator
  * @f: the function associated
  */
-typedef struct op 
+typedef struct op
 {
-	char *op;
-	int (*f)(int a, int b);
+        char *op;
+        int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
